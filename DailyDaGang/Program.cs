430aa89cbe30920612a59142ddb027ede83ca079
jiang <1982905003@qq.com>
2025-09-12 22:10:42 +0800

    重构项目

--- DailyDaGang/Program.cs
+++ DailyDaGang/Program.cs
@@ -1,6 +1,5 @@
 using DailyDaGang.Components;
 using DailyDaGang.Core;
-using SqlSugar;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,54 +12,6 @@ services.AddRazorComponents()
 services.AddAntDesign();
 
 services.AddHttpContextAccessor();
-//注册SqlSugar用AddScoped
-services.AddTransient<ISqlSugarClient>(s =>
-{
-    //Scoped用SqlSugarClient 
-    SqlSugarClient sqlSugar = new SqlSugarClient(new ConnectionConfig()
-    {
-        DbType = SqlSugar.DbType.Sqlite,
-        ConnectionString = "DataSource=outline.db",
-        IsAutoCloseConnection = true,
-        ConfigureExternalServices = new ConfigureExternalServices()
-        {
-            EntityService = (x, p) => //处理列名
-            {
-                //最好排除DTO类
-                p.DbColumnName = UtilMethods.ToUnderLine(p.DbColumnName);//驼峰转下划线方法
-                                                                         //支持string?和string  
-                if (p.IsPrimarykey == false && new NullabilityInfoContext()
-                 .Create(x).WriteState is NullabilityState.Nullable)
-                {
-                    p.IsNullable = true;
-                }
-            },
-
-            EntityNameService = (x, p) => //处理表名
-            {
-                //最好排除DTO类
-                if (p.DbTableName.EndsWith("Entity"))
-                    p.DbTableName = p.DbTableName.Substring(0, p.DbTableName.Length - 6);
-                p.DbTableName = UtilMethods.ToUnderLine(p.DbTableName);//驼峰转下划线方法
-            }
-        }
-    },
-   db =>
-   {
-       var types = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.GetCustomAttributes<SugarTable>().Any()).ToArray();
-       if(types.Any())
-           db.CodeFirst.InitTables(types);
-
-       db.Aop.OnLogExecuting = (sql, pars) =>
-       {
-           Console.WriteLine($"{DateTime.Now:HH:mm:ss}:{sql}");
-       };
-   }
-   );
-    return sqlSugar;
-});
-
-services.AddScoped(typeof(BaseRepository<>));
 
 var app = builder.Build();
 
